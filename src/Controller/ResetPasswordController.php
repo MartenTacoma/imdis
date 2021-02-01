7f580fb5a6c141034b8a857b1a44a55866d97499
Marten Tacoma <70998056+MartenTacoma@users.noreply.github.com>
2021-02-01 10:55:43 +0100

    redirect to account on login

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -122,7 +122,7 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
-            return $this->redirectToRoute('program_index');
+            return $this->redirectToRoute('user_self');
         }
 
         return $this->render('reset_password/reset.html.twig', [
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,7 +15,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-             return $this->redirectToRoute('program_index');
+             return $this->redirectToRoute('user_self');
         }
 
         // get the login error if there is one
