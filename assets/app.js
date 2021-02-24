524d4a7cefd767e8f42120bb68c66b689db83bb7
Marten Tacoma <70998056+MartenTacoma@users.noreply.github.com>
2021-02-24 07:57:42 +0100

    Inject world.svg, allowing caching

--- assets/app.js
+++ assets/app.js
@@ -15,24 +15,29 @@ import './bootstrap';
 const $ = require('jquery');
 $(function() {
     $('a[href^="https://"]').attr('target','_blank');
-    $('.worldMap svg').on('click', function(e){
-        $(this).parents('.sticky').addClass('fullMap');
-        e.stopPropagation();
-    });
-    $('.worldMap').on('click', function(e){
-        $(this).parents('.sticky').removeClass('fullMap');
-    });
-    $('.worldMap svg').on('mousemove', function(e){
-        $('#toolTip').css('top', e.pageY - $(document).scrollTop()).css('left', e.pageX + 20)
-    })
-    $('.worldMap path').on('mouseover', function(){
-        var str = $(this).attr('title');
-        if(typeof registrations != 'undefined' && typeof registrations[$(this).attr('id')] != 'undefined'){
-            str = str + ' - ' + registrations[$(this).attr('id')];
-        }
-        $('#toolTip').text(str).show();
-    });
-    $('.worldMap path').on('mouseout', function(){
-        $('#toolTip').text('').hide();
-    });
+    if ($('.worldMap').length){
+        $.get('/img/world.svg', function(r){
+            $('.worldMap .map svg').append([r.documentElement.children]);
+            $('.worldMap svg').on('click', function(e){
+                $(this).parents('.sticky').addClass('fullMap');
+                e.stopPropagation();
+            });
+            $('.worldMap').on('click', function(e){
+                $(this).parents('.sticky').removeClass('fullMap');
+            });
+            $('.worldMap svg').on('mousemove', function(e){
+                $('#toolTip').css('top', e.pageY - $(document).scrollTop()).css('left', e.pageX + 20)
+            })
+            $('.worldMap path').on('mouseover', function(){
+                var str = $(this).attr('title');
+                if(typeof registrations != 'undefined' && typeof registrations[$(this).attr('id')] != 'undefined'){
+                    str = str + ' - ' + registrations[$(this).attr('id')];
+                }
+                $('#toolTip').text(str).show();
+            });
+            $('.worldMap path').on('mouseout', function(){
+                $('#toolTip').text('').hide();
+            });
+        })
+    }
 });
