ea90454fae1665c0185f2343228b09745e6aabe8
Marten Tacoma <70998056+MartenTacoma@users.noreply.github.com>
2021-07-19 13:17:12 +0200

    Make theme tagline optional

--- src/Entity/Theme.php
+++ src/Entity/Theme.php
@@ -25,7 +25,7 @@ class Theme
     private $title;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $tagline;
 
